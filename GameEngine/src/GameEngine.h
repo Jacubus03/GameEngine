0e5e652784eeded5535703bfb4bc8e8a6d3ad483
Jacubus03 <jakubgorski1212@gmail.com>
2026-07-14 13:00:52 +0200

    Logging

--- GameEngine/src/GameEngine.h
+++ GameEngine/src/GameEngine.h
@@ -3,4 +3,6 @@
 // For use by GameEngine applications
 
 #include "GameEngine/Application.h"
+#include "GameEngine/Log.h"
+
 #include "GameEngine/EntryPoint.h"
--- GameEngine/src/GameEngine/EntryPoint.h
+++ GameEngine/src/GameEngine/EntryPoint.h
@@ -6,6 +6,10 @@ extern GameEngine::Application* GameEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	GameEngine::Log::Init();
+	LOG_CORE_ERROR("Initialized Log!");
+	LOG_INFO("Hello! Var={0}", 5);
+
 	auto app = GameEngine::CreateApplication();
 	app->Run();
 	delete app;
