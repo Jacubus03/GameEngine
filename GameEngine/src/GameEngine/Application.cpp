f77bf6502589362b673040fe067dbc7332dbd843
Jacubus03 <jakubgorski1212@gmail.com>
2026-09-04 21:59:47 +0200

    Event System

--- GameEngine/src/GameEngine/Application.cpp
+++ GameEngine/src/GameEngine/Application.cpp
@@ -1,4 +1,8 @@
 #include "Application.h"
+#include "GameEngine/Core.h"
+#include "GameEngine/Events/Event.h"
+#include "GameEngine/Events/ApplicationEvent.h"
+#include "GameEngine/Log.h"
 
 namespace GameEngine
 {
@@ -12,6 +16,9 @@ namespace GameEngine
 
 	void Application::Run()
 	{
+		WindowResizeEvent resizeEvent(1280, 720);
+		LOG_TRACE(resizeEvent.ToString());
+
 		while (true)
 		{
 			// Update and render the application
